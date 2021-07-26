36004c9e8096a25ea6bf58523718558c43958ecb
RodrigoMoreiraDaSilva <rodrigomoreiradasilva.of@gmail.com>
2021-07-26 07:57:39 -0300

    api update

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create(
     {
-        baseURL: 'https://my-json-server.typicode.com/rodrigsmor/Fake-server/produtos'
+        baseURL: 'https://my-json-server.typicode.com/rodrigsmor/e-commerce'
     }
 );
 
